a03287f711272e9ed30b1ac5d2668ad887fe6fd1
kamijooo <kamisayu426@gmail.com>
2026-09-01 17:19:22 +0900

    Update stores.js

--- src/_data/stores.js
+++ src/_data/stores.js
@@ -67,7 +67,7 @@ module.exports = {
     {
       region: "tokyo",
       slug: "ginza",
-      name_full: "Kobe Beef Wagyu Halal Hamburger Pizza & Steak (Vegan Gluten Free) Ginza Restaurant  银座 神户 和牛 餐厅",
+      name_full: "Kobe Beef Wagyu Halal Hamburger Pizza & Steak Vegan Ginza Restaurant 东京餐厅",
       hero_place: "Tokyo's Ginza",
       area_en: "Ginza",
       area_jp: "銀座",
